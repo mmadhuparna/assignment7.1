29b8009bce5284405cfb759adcbfd0d9aa03fa41
mmadhuparna <mmadhuparna@yahoo.com>
2018-08-16 16:17:27 +0530

    commit

---
+++ myscript.js
@@ -0,0 +1,28 @@
+//variable declaration
+var num1 = -8;
+var num2 = 12.5;
+var num3 = -6.769;
+var num4 = 0;
+var num5 = -1;
+
+//conditional statement
+if (num1>num2 && num1>num3 && num1>num4 && num1>num5)
+{
+    console.log(num1);
+}
+else if (num2>num1 && num2>num3 && num2>num4 && num2>num5)
+{
+    console.log(num2);
+}
+else if (num3>num1 && num3>num2 && num3>num4 && num3>num5)
+{
+    console.log(num3);
+}
+else if (num4>num1 && num4>num2 && num4>num3 && num4>num5)
+{
+    console.log(num4);
+}
+else
+{
+    console.log(num5);
+}
